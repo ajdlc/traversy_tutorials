62bd8b0cfb1d52f284a94a0a4ead4da315909f64
AJ De La Costa <ajdlc47@gmail.com>
2019-12-16 16:43:35 -0800

    Simple routes before using the Express router

--- Express JS Crash Course/index.js
+++ Express JS Crash Course/index.js
@@ -13,6 +13,22 @@ const app = express();
   // Gets all members
 app.get("/api/members", (req, res) => res.json(members));
 
+// Get single member
+app.get("/api/members/:id", (req, res) => {
+    // The :id above is a url parameter
+    // res.send(req.params.id);
+
+    const found = members.some(member => member.id === parseInt(req.params.id));
+
+    if (found) {
+        res.json(members.filter(member => {
+            return member.id === parseInt(req.params.id);
+        }))
+    } else {
+        res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
+    }
+})
+
 // Set Static Folder
 app.use(express.static(path.join(__dirname, "public")));
 
