e0f94e330251ad3810b5b0bf1f65e81da0657f66
AJ De La Costa <ajdlc47@gmail.com>
2019-12-19 15:50:35 -0800

    Completed

---
+++ Custom VS Code Snippets/app.js
@@ -0,0 +1,13 @@
+// Look at the javascript.json file for the snippets that were created.
+
+/**
+ * To access the snippets in VS Code, click ctrl + p, then type:
+ *  >snippets
+ * Then select:
+ *  Preferences Configure User Snippets
+ * 
+ * Then select your language of choice.
+ * 
+ * We also used the following link to generate the snippets:
+ *  https://snippet-generator.app/
+ */
