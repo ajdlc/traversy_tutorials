c2bd9b7694ad1a61bafd38b9303c05e514954c71
AJ De La Costa <ajdlc47@gmail.com>
2019-12-19 13:15:04 -0800

    Vue Router  and completed code.

---
+++ Vue.js Crash Course/vue_crash_todolist/src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, dolore maxime veritatis eaque beatae commodi vel cumque facilis? Corrupti natus soluta ea ut vero quas et beatae sapiente asperiores dignissimos, voluptatum eaque laudantium ad quam incidunt architecto optio harum facilis voluptatibus assumenda, impedit perferendis debitis quasi delectus. Quisquam aliquam dolor enim, necessitatibus dolore neque quas reiciendis dicta praesentium inventore iure, recusandae ipsa accusamus sit architecto. Reprehenderit necessitatibus delectus nemo accusamus ex hic, voluptates nulla, doloremque quaerat fuga debitis reiciendis tempore iure numquam sit dolorem iste, velit libero dolorum alias possimus commodi! Provident dolor dolorum enim et non odio ratione quod!</p>
+  </div>
+</template>
